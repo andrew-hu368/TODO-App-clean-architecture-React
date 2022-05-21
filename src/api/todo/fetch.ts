d59260e022b286baad70121d9d777dc1a7e6b433
andrew-hu368 <hu.lha86@gmail.com>
2022-05-21 21:20:02 +0200

    fixed api

--- src/api/todo/fetch.ts
+++ src/api/todo/fetch.ts
@@ -7,38 +7,69 @@ import type {
 
 import type { Todo } from '../../domain/todo';
 
+interface UnformattedTodo {
+  id: string;
+  completed: boolean;
+  title: string;
+}
+
 const url = 'https://jsonplaceholder.typicode.com/todos';
 
 export const createRemoteTodo: ICreateRemoteTodo = async (body) => {
   const res = await fetch(url, {
     method: 'POST',
-    body: JSON.stringify(body),
+    body: JSON.stringify({
+      title: body.todo,
+    }),
   });
+  // api only returns an ID, the just ensures we are passing TS-check
+  const unformattedTodo: UnformattedTodo = await res.json();
 
-  return res.json();
+  return {
+    ...unformattedTodo,
+    todo: body.todo,
+    completed: false,
+  };
 };
 
-export const updateTodo: IUpdateRemoteTodo = async (todo: Todo) => {
+export const updateRemoteTodo: IUpdateRemoteTodo = async (todo: Todo) => {
   const res = await fetch(url + `/${todo.id}`, {
     method: 'PUT',
-    body: JSON.stringify(todo),
+    body: JSON.stringify({
+      ...todo,
+      title: todo.todo,
+    }),
   });
+  // api only returns an ID
+  await res.json();
 
-  return res.json();
+  return {
+    ...todo,
+  };
 };
 
-export const deleteTodo: IDeleteRemoteTodo = async ({ id }) => {
+export const deleteRemoteTodo: IDeleteRemoteTodo = async ({ id }) => {
   const res = await fetch(url + `/${id}`, {
     method: 'DELETE',
   });
+  // api only returns an ID, the just ensures we are passing TS-check
+  const unformattedTodo: UnformattedTodo = await res.json();
 
-  return res.json();
+  return {
+    ...unformattedTodo,
+    todo: unformattedTodo.title,
+    id,
+  };
 };
 
-export const fetchAllTodos: IGetRemoteTodos = async () => {
+export const getRemoteTodos: IGetRemoteTodos = async () => {
   const res = await fetch(url, {
     method: 'GET',
   });
+  const unformattedTodos: UnformattedTodo[] = await res.json();
 
-  return res.json();
+  return unformattedTodos.map((item) => ({
+    ...item,
+    todo: item.title,
+  }));
 };
